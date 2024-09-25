3f2944b6e0ff1ee72b727b04500ce25d163f2ba1
devITae <sky@mberd.com>
2024-09-26 03:39:41 +0900

    Add Category Menu UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
 import { useState, useEffect } from 'react'
+import tw from 'twin.macro'
+import styled from 'styled-components'
 import { pos } from './positions.json'
-import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk' // Import the InfoWindow component
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
 import Marker from './assets/marker.png'
 
+
+const CategoryItem = styled.li`
+  float: left;
+  list-style: none;
+  width: 50px;
+  border-right: 1px solid #acacac;
+  padding: 6px 0;
+  text-align: center;
+  cursor: pointer;
+  
+  &.on {
+    background: #eee;
+  }
+  
+  &:hover {
+    background: #ffe6e6;
+    border-left: 1px solid #acacac;
+    margin-left: -1px;
+  }
+  
+  &:last-child {
+    margin-right: 0;
+    border-right: 0;
+  }
+  
+  span {
+    display: block;
+    margin: 0 auto 3px;
+    width: 27px;
+    height: 28px;
+  }
+  
+  .category_bg {
+    background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png) no-repeat;
+  }
+  
+  .bank {
+    background-position: -10px 0;
+  }
+  
+  .mart {
+    background-position: -10px -36px;
+  }
+  
+  .pharmacy {
+    background-position: -10px -72px;
+  }
+  
+  .oil {
+    background-position: -10px -108px;
+  }
+  
+  .cafe {
+    background-position: -10px -144px;
+  }
+  
+  .store {
+    background-position: -10px -180px;
+  }
+  
+  &.on .category_bg {
+    background-position-x: -46px;
+  }
+`
+
 function App() {
-  //const [selectedCategory, setSelectedCategory] = useState("coffee")
-  //const markerImageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/category.png"
-  //const imageSize = { width: 22, height: 26 }
-  //const spriteSize = { width: 36, height: 98 }
+
   const [isVisibleId, setIsVisibleId] = useState<string | null>(null)
 
   function success(pos) {
@@ -21,7 +85,6 @@ function App() {
     console.warn(`Error: ${error.message}`)
   }
 
-  //const map = useMap()
   const [state, setState] = useState({
     center: {
       lat: 37.29781,
@@ -30,6 +93,11 @@ function App() {
     errMsg: null,
     isLoading: true,
   })
+
+  const [selectedCategory, setSelectedCategory] = useState("entire")
+  //const markerImageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/category.png"
+  //const imageSize = { width: 22, height: 26 }
+  //const spriteSize = { width: 36, height: 98 }
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -147,45 +215,88 @@ function App() {
   }
   
   return (
-    <div id='mapwrap' className='w-full h-full'>
-      {/* 지도 위에 표시될 마커 카테고리 */}
-      <Map
-        id='map'
-        center={{ lat: 37.29781, lng: 126.835358 }}   // 지도의 중심 좌표
-        style={{'width': '100%', 'height': '100vh'}} // 지도 크기
-        level={3}                                   // 지도 확대 레벨
-      >
-        {
-          pos.map((value, index) => (
-            <EventMarkerContainer
-              key={`EventMarkerContainer-${value.lat}-${value.lng}`}
-              id={`marker-${index}`}
-              position={{ lat: value.lat, lng: value.lng }}
-              content={value.title}
-              wheel={value.wheel}
-              elevator={value.elevator}
-              toilet={value.toilet}
-              parking={value.parking}
-              dots={value.dots}
+    <>
+      <div id='mapwrap' className='w-full h-full'>
+        {/* 지도 위에 표시될 마커 카테고리 */}
+        <Map
+          id='map'
+          center={{ lat: 37.29781, lng: 126.835358 }}   // 지도의 중심 좌표
+          style={{'width': '100%', 'height': '100vh'}} // 지도 크기
+          level={3}                                   // 지도 확대 레벨
+          minLevel={4}                                // 지도 최소 레벨
+          maxLevel={2}                               // 지도 최대 레벨
+        >
+          {pos.map((value, index) => {
+            const showMarker =
+            selectedCategory === "entire" ||
+            (selectedCategory === "wheel" && value.wheel) ||
+            (selectedCategory === "elevator" && value.elevator) ||
+            (selectedCategory === "toilet" && value.toilet) ||
+            (selectedCategory === "parking" && value.parking)
+            
+            return (
+              showMarker && (
+                <EventMarkerContainer
+                  key={`EventMarkerContainer-${value.lat}-${value.lng}`}
+                  id={`marker-${index}`}
+                  position={{ lat: value.lat, lng: value.lng }}
+                  content={value.title}
+                  wheel={value.wheel}
+                  elevator={value.elevator}
+                  toilet={value.toilet}
+                  parking={value.parking}
+                  dots={value.dots}
+                />
+              )
+            )
+          })}
+          {/* 현재 위치 표시 마커 */}
+          {!state.isLoading && (
+            <MapMarker
+              image={{
+                src: "https://t1.daumcdn.net/localimg/localimages/07/2018/mw/m640/ico_marker.png",
+                size: { width: 30, height: 30 },
+              }}
+              position={state.center}
             />
-          ))
-        }
-        /* 현위치 마커 */
-        {!state.isLoading && (
-          <MapMarker
-            image={{
-              src: "https://t1.daumcdn.net/localimg/localimages/07/2018/mw/m640/ico_marker.png",
-              size: { width: 30, height: 30 },
-            }}
-            position={state.center}
-          />
-        )}
-      </Map>
-      <div className="chat">
-
+          )}
+        </Map>
+        {/* 지도 위에 표시될 마커 카테고리 */}
+        <ul className='absolute top-[10px] left-[10px] rounded-md border border-[#909090] shadow-md bg-white overflow-hidden z-[2]'>
+          <CategoryItem
+            onClick={() => setSelectedCategory("entire")}
+            className={selectedCategory === "entire" ? "on" : ""}
+          >
+            전체
+          </CategoryItem>
+          <CategoryItem
+            onClick={() => setSelectedCategory("parking")}
+            className={selectedCategory === "parking" ? "on" : ""}
+          >
+            장애인 주차장
+          </CategoryItem>
+          <CategoryItem
+            onClick={() => setSelectedCategory("toilet")}
+            className={selectedCategory === "toilet" ? "on" : ""}
+          >
+            장애인 화장실
+          </CategoryItem>
+          <CategoryItem
+            onClick={() => setSelectedCategory("elevator")}
+            className={selectedCategory === "elevator" ? "on" : ""}
+          >
+            엘레베이터
+          </CategoryItem>
+          <CategoryItem
+            onClick={() => setSelectedCategory("ramp")}
+            className={selectedCategory === "ramp" ? "on" : ""}
+          >
+            경사로
+          </CategoryItem>
+        </ul>
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
 export default App
