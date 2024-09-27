2eab978133538c3ac22f807e438331176e390058
devITae <sky@mberd.com>
2024-09-27 13:24:17 +0900

    Remove useless code

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import mkcert from'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  server: {
-    historyApiFallback: true,
-    https: true,
-  },
   plugins: [react(), mkcert()],  
 })
