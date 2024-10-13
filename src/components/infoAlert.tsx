f653df8589241053e9273f797bc7b459c64331d4
devITae <sky@mberd.com>
2024-10-13 18:21:46 +0900

    Modify the text of information

--- src/components/infoAlert.tsx
+++ src/components/infoAlert.tsx
@@ -10,11 +10,12 @@ const InfoAlert: React.FC<InfoAlertProps> = ({ onClose }) => {
       <div className="bg-white p-5 rounded-md shadow-lg w-80 text-sm">
         <h2 className="text-lg font-semibold mb-3">정보</h2>
         <p className='mb-4'>한양대학교 ERICA캠퍼스의 배리어프리맵 웹서비스인 '오픈하냥' 입니다.</p>
+        <p>[개발자]</p>
         <p>Frontend: 이재형 (컴퓨터학부 21)</p>
         <p>Backend: 정윤성 (컴퓨터학부 20)</p>
         <p>Designer: 임동섭 (경영학부 21)</p>
-        <p className='mt-4'>도움 : ERICA 인권센터</p>
-        <p>문의 : <></>
+        <p className='mt-4'>[도움]<br/>한양대학교 ERICA 인권센터</p>
+        <p className='mt-4'>[문의]<br/>
           <a className='text-blue-500' href='mailto:alpha@hanyang.ac.kr'>
             alpha@hanyang.ac.kr
           </a>
