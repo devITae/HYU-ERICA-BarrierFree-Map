b008c6d3746de6323ce4e9a4efb60aada4d8f0b3
devITae <sky@mberd.com>
2024-10-14 09:51:33 +0900

    Add settings for path error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import mkcert from'vite-plugin-mkcert'
 import macrosPlugin from 'vite-plugin-babel-macros'
 import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     react(), 
     mkcert(), 
     macrosPlugin(),
+    tsconfigPaths(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
